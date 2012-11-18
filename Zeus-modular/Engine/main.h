016fc5f978a0569bfc022f09360ac7f7ef2d1919
clucasa <clucasa@onid.orst.edu>
2012-11-17 21:32:22 -0800

    Modularization basically complete. Need to modularize different shader types.
    Added a basic obj file loader, phong shading, texturing from file, alpha blending based on pased in alpha value or from alpha map image. Kills the pixel if alpha is less than 0.1.

--- Zeus-modular/Engine/main.h
+++
@@ -1,7 +0,0 @@
-// include the basic windows header files and the Direct3D header files
-#include <windows.h>
-#include <windowsx.h>
-
-#include "geometryclass.h"
-#include "objectclass.h"
-#include "shaderclass.h"
--- Zeus-modular/README.md
+++
@@ -1,7 +0,0 @@
-Zeus
-====
-
-The main repository for our graphics engine.
-
-To run this:
-1. Make sure you have updated Directx11 drivers!
